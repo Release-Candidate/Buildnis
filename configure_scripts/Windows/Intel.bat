8581f9a782ba1c3210e1e37bb7821a8628fdfa5c
Release-Candidate <rec@gmx.at>
2021-02-15 23:25:50 +0100

    Added windows config scripts

---
+++ configure_scripts/Windows/Intel.bat
@@ -0,0 +1,72 @@
+:: SPDX-License-Identifier: MIT
+:: Copyright (C) 2021 Roland Csaszar
+::
+:: Project:  Buildnis
+:: File:     Intel.bat
+:: Date:     15.Feb.2021
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+:: searches for the Intel compilers setting batch file.
+
+@echo off
+
+:: ONEAPI_ROOT
+:: C:\Program Files (x86)\Intel\oneAPI\
+
+:: Intel needs intel64 or ia32 as arguments to set 64 or 32 bit compilers
+set X64_ARGUMENT=intel64
+set I86_ARGUMENT=ia32
+
+:: parse command line arguments
+:PARSE_ARGS
+if /i "%1"==""    goto :END_PARSE_ARGS
+if /i "%1"=="x64" (set CMD_ARG=%X64_ARGUMENT%) & shift & goto :PARSE_ARGS
+if /i "%1"=="x86" (set CMD_ARG=%I86_ARGUMENT%) & shift & goto :PARSE_ARGS
+:END_PARSE_ARGS
+
+:: JSON output
+echo {
+echo "build_tools": 
+echo [
+echo     {
+echo         "name": "Intel C++ Classic",
+echo         "name_long": "",
+echo         "version": "",
+echo         "build_tool_exe": "icl",
+echo         "install_path": "",
+echo         "env_script": ""
+echo     },
+echo     {
+echo         "name": "Intel DPC++/C++",
+echo         "name_long": "",
+echo         "version": "",
+echo         "build_tool_exe": "icx",
+echo         "install_path": "",
+echo         "env_script": ""
+echo     },
+echo     {
+echo         "name": "Intel Fortran Classic",
+echo         "name_long": "",
+echo         "version": "",
+echo         "build_tool_exe": "ifort",
+echo         "install_path": "",
+echo         "env_script": ""
+echo     },
+echo     {
+echo         "name": "Intel Fortran (ifx)",
+echo         "name_long": "",
+echo         "version": "",
+echo         "build_tool_exe": "ifx",
+echo         "install_path": "",
+echo         "env_script": ""
+echo     },
+echo     {
+echo         "name": "Intel Python",
+echo         "name_long": "",
+echo         "version": "",
+echo         "build_tool_exe": "python",
+echo         "install_path": "",
+echo         "env_script": ""
+echo     }
+echo ]
+echo }
---
+++ configure_scripts/Windows/VisualStudio.bat
@@ -0,0 +1,119 @@
+:: SPDX-License-Identifier: MIT
+:: Copyright (C) 2021 Roland Csaszar
+::
+:: Project:  Buildnis
+:: File:     VisualStudio.bat
+:: Date:     15.Feb.2021
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+:: Searches for Visual Studio 2017/2019 (and newer) installations, outputs the
+:: gathered information JSON formatted.
+
+@echo off
+
+:: Commandline to call `vswhere` whith, the MS program to search for Visual
+:: Studio installations and their properties.
+set VS_WHERE="%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe"
+set VS_WHERE_CMDLINE=%VS_WHERE% -prerelease
+
+
+:: VS needs AMD64 or x86 as arguments to set 64 or 32 bit compilers
+set X64_ARGUMENT=-arch=amd64
+set I86_ARGUMENT=-arch=x86
+
+:: parse command line arguments
+:PARSE_ARGS
+if /i "%1"==""    goto :END_PARSE_ARGS
+if /i "%1"=="x64" (set CMD_ARG=%X64_ARGUMENT%) & shift & goto :PARSE_ARGS
+if /i "%1"=="x86" (set CMD_ARG=%I86_ARGUMENT%) & shift & goto :PARSE_ARGS
+:END_PARSE_ARGS
+
+setlocal enabledelayedexpansion
+
+:: get installation path
+set /a IDX=0
+for /f "delims=" %%l in ('%VS_WHERE_CMDLINE% -property installationPath') do (
+    set RESULT[!IDX!].install_path=%%l
+    call :TRIM RESULT[!IDX!].install_path
+    set /a "IDX+=1"
+)
+
+:: get the 'short' name
+set /a IDX=0
+for /f "delims=" %%l in ('%VS_WHERE_CMDLINE% -property catalog_productName') do (
+    set RESULT[!IDX!].name=%%l
+    call :TRIM RESULT[!IDX!].name
+    set /a "IDX+=1"
+)
+
+:: get the year part of VS' name
+set /a IDX=0
+for /f "delims=" %%l in ('%VS_WHERE_CMDLINE% -property catalog_productLineVersion') do (
+    set RESULT[!IDX!].year=%%l
+    call :TRIM RESULT[!IDX!].year
+    set /a "IDX+=1"
+)
+
+:: full product name
+set /a IDX=0
+for /f "delims=" %%l in ('%VS_WHERE_CMDLINE% -property displayName') do (
+    set RESULT[!IDX!].name_long=%%l
+    call :TRIM RESULT[!IDX!].name_long
+    set /a "IDX+=1"
+)
+
+:: version string
+set /a IDX=0
+for /f "delims=" %%l in ('%VS_WHERE_CMDLINE% -property catalog_productDisplayVersion') do (
+    set RESULT[!IDX!].version=%%l
+    call :TRIM RESULT[!IDX!].version
+    set /a "IDX+=1"
+)
+
+:: check environment script, if it exists, add it
+set /a LOOP_IDX=0
+:LOOP1
+if defined RESULT[!LOOP_IDX!].install_path (
+    call set a=%%RESULT[!LOOP_IDX!].install_path%%\Common7\Tools\VsDevCmd.bat
+    if exist "!a!" (
+        set RESULT[!LOOP_IDX!].env_script=!a!
+        call :TRIM RESULT[!LOOP_IDX!].env_script
+    )
+    set /a "LOOP_IDX+=1"
+    goto :LOOP1
+)
+
+:: JSON output
+echo {
+echo "build_tools":
+echo [
+set /a LOOP_IDX=0
+:LOOP
+if defined RESULT[!LOOP_IDX!].install_path (
+    echo     {
+    call echo         "name": "%%RESULT[!LOOP_IDX!].name%% %%RESULT[!LOOP_IDX!].year%%",
+    call echo         "name_long": "%%RESULT[!LOOP_IDX!].name_long%% %%RESULT[!LOOP_IDX!].version%%",
+    call echo         "version": "",
+    call echo         "build_tool_exe": "cl",
+    call echo         "install_path": "%%RESULT[!LOOP_IDX!].install_path%%",
+    call echo         "env_script": "%%RESULT[!LOOP_IDX!].env_script%%",
+    set /a "LOOP_IDX+=1"
+    echo     },
+    goto :LOOP
+)
+echo ]
+echo }
+
+EndLocal
+GOTO :EOF
+
+:: trim spaces off the strings
+:TRIM
+SetLocal EnableDelayedExpansion
+Call :TRIMSUB %%%1%%
+EndLocal & set %1=%tempvar%
+GOTO :EOF
+
+:TRIMSUB
+set tempvar=%*
+GOTO :EOF
