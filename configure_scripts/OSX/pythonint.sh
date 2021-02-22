117c5001247fc7d08090921aaeab4bb7d9eca305
Release-Candidate <rec@gmx.at>
2021-02-22 23:43:25 +0100

    Added OS X build tool scripts

---
+++ configure_scripts/OSX/pythonint.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Copyright (C) 2021 Roland Csaszar
+#
+# Project:  Buildnis
+# File:     pythonint.sh
+# Date:     22.Feb.2021
+################################################################################
+
+# assumes python is in the PATH
+PYTHON_VERSION=$(python3 --version 2>&1)
+PYTHON_PATH=$(dirname `which python3`)
+
+echo "{"
+echo '    "build_tools":'
+echo "    ["
+echo "         {"
+echo "            \"name\": \"Python\","
+echo "            \"name_long\": \"${PYTHON_VERSION}\","
+echo "            \"version\": \"\","
+echo "            \"version_arg\": \"--version\","
+echo "            \"version_regex\": \"^\\\\S+ (.*)\","
+echo "            \"build_tool_exe\": \"python3\","
+echo "            \"install_path\": \"${PYTHON_PATH}\","
+echo "            \"env_script\": \"\""
+echo "         }"
+echo "    ]"
+echo "}"
