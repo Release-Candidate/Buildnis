e78b5970f2c3b0270930a6a08ff81b3d96a09e26
Release-Candidate <rec@gmx.at>
2021-03-08 13:54:38 +0100

    Removed unneeded if

--- buildnis/modules/helpers/files.py
+++ buildnis/modules/helpers/files.py
@@ -265,10 +265,8 @@ def areHashesSame(
         hash1 = hashFile(file1)
         hash2 = hashFile(file2)
 
-        if hash1 == hash2:
-            return True
-        else:
-            return False
+        return hash1 == hash2
+
     except Exception as excp:
         raise FileCompareException(excp)
 
