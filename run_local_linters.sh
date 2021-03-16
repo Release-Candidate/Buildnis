7d50fe861c177c207b7a4a209ffb84b3c000b127
Release-Candidate <rec@gmx.at>
2021-03-16 16:16:04 +0100

    Add tests

--- run_local_linters.sh
+++ run_local_linters.sh
@@ -8,9 +8,10 @@
 ################################################################################
 
 # Runs the local linters
-black buildnis
-pyflakes buildnis
-pycodestyle buildnis
-pydocstyle buildnis
-flake8 buildnis
-bandit -r buildnis
+black buildnis test_project
+pyflakes buildnis test_project
+pycodestyle buildnis test_project
+pydocstyle buildnis test_project
+flake8 buildnis test_project
+isort buildnis test_project
+bandit -r buildnis test_project
---
+++ test_project/test_project.py
@@ -0,0 +1,129 @@
+# SPDX-License-Identifier: MIT
+# Copyright (C) 2021 Roland Csaszar
+#
+# Project:  Buildnis
+# File:     test_project.py
+# Date:     16.Mar.2021
+###############################################################################
+
+from __future__ import annotations
+
+import os
+import pathlib
+import runpy
+import sys
+from typing import List
+
+from hypothesis import given, settings
+from hypothesis import strategies as st
+
+# import pytest
+
+
+################################################################################
+def runBuildnis(cmd_line_args: List[str]) -> None:
+    """Run the program with the given arguments.
+
+    Returns the output of the command in the tuple `stdout, stderr`. The first element
+    of the tuple holds the process' output on `stdout`, the second the output on
+    `stderr`.
+
+    Args:
+        cmd_line_args (List[str]): The arguments to pass to `buildnis`.
+    """
+    test_project_path = pathlib.Path(__file__).parent.absolute()
+    root_dir = os.path.normpath(test_project_path.parent.absolute())
+    sys.path.insert(0, root_dir)
+
+    sys_argv_list = [""]
+    sys_argv_list.extend(cmd_line_args)
+    sys_argv_list.append("./test_project/project_config.json")
+    sys.argv = sys_argv_list
+
+    runpy.run_module("buildnis", run_name="__main__")
+
+
+################################################################################
+def test_runVersion() -> None:
+    """Run `buildnis` with the argument `--version` to show the version information."""
+    try:
+        runBuildnis(["--version"])
+    except SystemExit as excp:
+        assert str(excp) == "0"  # nosec
+
+
+################################################################################
+def test_runHelp() -> None:
+    """Run `buildnis` with the argument `--help` to show the usage information."""
+    try:
+        runBuildnis(["--help"])
+    except SystemExit as excp:
+        assert str(excp) == "0"  # nosec
+
+
+################################################################################
+@settings(max_examples=50, deadline=None)
+@given(
+    arg=st.sampled_from(elements=["-q", "", "-v", "-vv"]), conf_out=st.text(max_size=20)
+)
+def test_runFirstTime(arg: str, conf_out: str) -> None:
+    """Run Buildnis for the first time.
+
+    Args:
+        arg (str): The verbosity argument.
+        conf_out (str): The path to the generated configuration directory.
+    """
+    try:
+        arg_list = []
+        if arg != "":
+            arg_list.append(arg)
+        if conf_out != "":
+            arg_list.append("--generated-conf-dir")
+            arg_list.append(conf_out)
+
+        runBuildnis(arg_list)
+    except SystemExit as excp:
+        assert str(excp) == "0"  # nosec
+
+
+################################################################################
+def test_runClean() -> None:
+    """Run `buildnis` with the argument `--clean` to remove generated build data"""
+    try:
+        runBuildnis(["--clean"])
+    except SystemExit as excp:
+        assert str(excp) == "0"  # nosec
+
+
+################################################################################
+@settings(max_examples=50, deadline=None)
+@given(arg=st.sampled_from(elements=["-q", "", "-v", "-vv"]))
+def test_runSecondTime(arg: str) -> None:
+    """Run Buildnis for the second time.
+
+    Args:
+        arg (str): The verbosity argument.
+    """
+    try:
+        arg_list = []
+        if arg != "":
+            arg_list.append(arg)
+
+        runBuildnis(arg_list)
+    except SystemExit as excp:
+        assert str(excp) == "0"  # nosec
+
+
+################################################################################
+def test_runDistClean() -> None:
+    """Run `buildnis` with the argument `--distclean` to remove  all generated data"""
+    try:
+        runBuildnis(["--distclean"])
+    except SystemExit as excp:
+        assert str(excp) == "0"  # nosec
+
+
+################################################################################
+if __name__ == "__main__":
+    test_runVersion()
+    test_runHelp()
