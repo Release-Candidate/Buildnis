705a1343b7b2c8b9492598545eb220e183d66eee
Release-Candidate <rec@gmx.at>
2021-03-18 00:44:56 +0100

    Added test directory

--- run_local_linters.bat
+++ run_local_linters.bat
@@ -7,10 +7,10 @@
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 :: Runs the local linters
-black buildnis test_project
-pyflakes buildnis test_project
-pycodestyle buildnis test_project
-pydocstyle buildnis test_project
-flake8 buildnis test_project
-isort buildnis test_project
-bandit -r buildnis test_project
+black buildnis test_project tests
+pyflakes buildnis test_project tests
+pycodestyle buildnis test_project tests
+pydocstyle buildnis test_project tests
+flake8 buildnis test_project tests
+isort buildnis test_project tests
+bandit -r buildnis test_project tests
